93713a55ab8900609667bfafebc6bd4ddc6777cc
eliakim-ads <eliakim.ldo@gmail.com>
2026-03-11 19:46:05 -0300

    Subindo versão Atualizada

--- src/main/java/com/github/app/controller/MedicoController.java
+++ src/main/java/com/github/app/controller/MedicoController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.app.model.medico.DadosCadastroMedico;
 
-@RestController // SPRING WEB - informa para o Springboot que abaixo é uma classe controladora de requisições (GET-POST_PUT-DELETE).
-@RequestMapping("medicos") // SPRING WEB - Cria um caminho (endpoint) para classe.
+@RestController // SPRINGWEB - informa para o Springboot que abaixo é uma classe controladora de requisições (GET-POST_PUT-DELETE).
+@RequestMapping("medicos") // SPRINGWEB - Cria um caminho (endpoint) para classe.
 public class MedicoController {
     
     @PostMapping // SPRING WEB - Informa que o método abaixo é do tipo POST (cadastrar).
